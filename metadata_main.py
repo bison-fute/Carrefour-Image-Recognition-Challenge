dc38daf9215e2ec599860056431f5a4e7230304d
bison-fute <75632298+bison-fute@users.noreply.github.com>
2021-03-14 15:25:24 +0100

    Add files via upload

---
+++ metadata_main.py
@@ -0,0 +1,18 @@
+import pickle as pk
+from metadata_processing import *
+
+# Metadata Processing
+min_cat_size = 50
+
+# set up eda object
+eda = ExploratoryDataAnalysis('metadata_images.json', min_cat_size)
+
+# get barcode to cat dictionary
+barcode_to_cat = eda.get_barcode_to_cat()
+cat_to_name = eda.get_cat_to_name()
+eda.plot_distrib()
+
+# writing the dictionary in memory:
+pk.dump(barcode_to_cat, open('barcode_to_cat', 'wb'))
+pk.dump(cat_to_name, open('cat_to_name', 'wb'))
+
